31fc20e25a6e709c179868e647c1161bd19bb3e1
randycph <randycph@gmail.com>
2026-09-19 10:21:30 +0800

    failed

--- server/ci-demo.test.js
+++ server/ci-demo.test.js
@@ -1,3 +1,4 @@
 test('CI failure switch is off during normal builds', () => {
-  expect(process.env.SIMULATE_CI_FAILURE).not.toBe('true')
+  // expect(process.env.SIMULATE_CI_FAILURE).not.toBe('true')
+  expect(true).toBe(false)
 })
